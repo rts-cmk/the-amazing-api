b4f8ad7f21660da9cc483f70c3875f749234ab3d
Brian Emilius <be@rts.dk>
2025-04-22 10:19:08 +0200

    add middleware for editor-or-admin checks

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,7 +1,8 @@
 import { getAllProducts, createProduct, getProductBySlug } from "../controllers/product.controller.js"
+import { isAuthenticated, isEditorOrAdmin } from "../middleware/auth.middleware.js"
 
 export default function (router) {
-	router.post("/api/v1/products", createProduct)
+	router.post("/api/v1/products", isAuthenticated, isEditorOrAdmin, createProduct)
 	router.get("/api/v1/products", getAllProducts)
 	router.get("/api/v1/products/:slug", getProductBySlug)
 }
